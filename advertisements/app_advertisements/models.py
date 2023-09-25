c3be8e087059ff0d22e58b2963f2ddba686a4aac
AngelinShp <angelinashpspb@gmail.com>
2023-09-25 18:53:19 +0300

    Админ-панель (код с урока)

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
 from django.db import models
-
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
+
+
+
 class Advertisement(models.Model):
     title = models.CharField(verbose_name="Заголовок", max_length=128)
     description = models.TextField("Описание")
@@ -9,6 +14,17 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date()==timezone.now().date():
+            created_time=self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; '
+                               'font-weight: bold">Сегодня в '
+                               '{}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
