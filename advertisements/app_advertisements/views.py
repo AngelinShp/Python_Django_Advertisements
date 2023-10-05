ea9460a6640082ed3098d016a9d3fb0265c9662c
AngelinShp <angelinashpspb@gmail.com>
2023-10-05 18:57:06 +0300

    Формы (ДЗ) + Аутентификация (код с урока)

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,31 +1,34 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from .models import Advertisement
-from .forms import AdvertisementForms
+from .forms import AdvertisementForm
 
 
 def index(request):
     advertisements=Advertisement.objects.all()
     context={'advertisements':advertisements}
-    return render(request, 'index.html', context)
+    return render(request, 'app_advertisements/index.html', context)
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advertisements/top-sellers.html')
 
+@login_required(login_url=reverse_lazy('login'))
 def adv_post(request):
     if request.method=="POST":
-        form = AdvertisementForms(request.POST, request.FILES)
+        form = AdvertisementForm(request.POST, request.FILES)
         if form.is_valid():
-            advertisement=Advertisement(**form.cleaned_data)
-            advertisement.user=request.user
-            advertisement.save()
+            new_advertisement=form.save(commit=False)
+            new_advertisement.user=request.user
+            new_advertisement.save()
             url = reverse('main_page')
             return redirect(url)
     else:
-        form = AdvertisementForms()
+        form = AdvertisementForm()
     context = {'form': form}
-    return render(request, 'advertisement-post.html', context)
+    return render(request, 'app_advertisements/advertisement-post.html', context)
