6e0ee201579e15ea7aa836a3643ae04024e89b35
Angelin <angelinashpspb@gmail.com>
2023-10-09 20:21:55 +0300

    Законченный проект

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.html import format_html
 from django.contrib.auth import get_user_model
@@ -43,7 +44,8 @@ class Advertisement(models.Model):
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
-
+    def get_absolute_url(self):
+        return reverse('adv_detail', kwargs={'pk': self.pk})
     class Meta:
         db_table = "advertisements"
 
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db.models import Count
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -11,12 +13,20 @@ from .forms import AdvertisementForm
 
 
 def index(request):
-    advertisements=Advertisement.objects.all()
-    context={'advertisements':advertisements}
+    title=request.GET.get('query')
+    print(title)
+    if title:
+        advertisements=Advertisement.objects.filter(title__icontains=title)
+    else:
+        advertisements = Advertisement.objects.all()
+    context={'advertisements':advertisements,
+             'title': title}
     return render(request, 'app_advertisements/index.html', context)
 
 def top_sellers(request):
-    return render(request, 'app_advertisements/top-sellers.html')
+    users=User.objects.annotate(adv_count=Count('advertisement')).order_by('-adv_count')
+    context={'users': users}
+    return render(request, 'app_advertisements/top-sellers.html', context)
 
 @login_required(login_url=reverse_lazy('login'))
 def adv_post(request):
@@ -32,3 +42,8 @@ def adv_post(request):
         form = AdvertisementForm()
     context = {'form': form}
     return render(request, 'app_advertisements/advertisement-post.html', context)
+
+def adv_detail(request, pk):
+    advertisements = Advertisement.objects.get(id=pk)
+    context = {'advertisements': advertisements}
+    return render(request, 'app_advertisements/advertisement.html', context)
